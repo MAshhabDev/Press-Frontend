695a043a45fdfde3c40cf58d05418cfa3668a8ea
Mahir4098 <mahirjr98@gmail.com>
2026-07-21 20:22:37 +0600

    create interactive DislikeButton client component

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import DislikeButton from "@/app/ui/DislikeButton";
+
+const BlogsSlugPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
+  return (
+    <div>
+      BlogsSlugPage : {slug}
+      <DislikeButton blogSlug={slug}></DislikeButton>
+    </div>
+  );
+};
+
+export default BlogsSlugPage;
