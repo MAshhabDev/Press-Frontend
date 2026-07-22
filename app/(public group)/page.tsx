7741f9b958dd1618a7712e1bba707e97a44a9b77
Mahir4098 <mahirjr98@gmail.com>
2026-07-22 18:34:01 +0600

    Install shadcn and Implement the right structure

--- app/(public group)/page.tsx
+++ app/(public group)/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const homePage = () => {
-  return <div>homePage</div>;
+  return (
+    <div>
+      Hello, Next.js!
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default homePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -9,16 +9,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={("h-full antialiased", "font-sans", inter.variable)}
-    >
-      <body className="min-h-full flex flex-col">
-        {/* Navbar */}
+    <html lang="en">
+      <body className={inter.className}>
         {children}
-
-        {/* Footer */}
-        </body>
+      </body>
     </html>
   );
 }
