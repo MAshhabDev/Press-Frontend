67c27e66b865f2bf1d27e5809d4a30e824440e7a
Mahir4098 <mahirjr98@gmail.com>
2026-07-25 13:47:11 +0600

    implment authentication and role based  authorization

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,68 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Public_Sans } from "next/font/google";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
+const AUTH_ROUTES = ["/login", "/register"];
+// const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"]
+const PUBLIC_ROUTES = ["/", "/news"];
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  return NextResponse.redirect(new URL("/home", request.url));
+
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const decodeToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+
+  let userRole = null;
+
+  if (decodeToken) {
+    userRole = decodeToken.role;
+    console.log(userRole);
+  }
+
+  //user is logged in and trying to access login or register page, redirect to dashboard or root home page
+
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    }
+    if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    }
+  }
+
+  const isPublic = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuth = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  // Authenticated page protection
+  if (!accessToken && !isPublic && !isAuth) {
+    return NextResponse.redirect(new URL("/login ", request.url));
+  }
+
+  //   Role Based Authorization
+
+  if (pathname.startsWith("/dashboard") && userRole !== "USER") {
+    return NextResponse.redirect(new URL("/not-found ", request.url));
+  }
+  if (pathname.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found ", request.url));
+  }
+  if (pathname.startsWith("/author-dashboard") && userRole !== "AUTHOR") {
+    return NextResponse.redirect(new URL("/not-found ", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: "/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)",
 };
